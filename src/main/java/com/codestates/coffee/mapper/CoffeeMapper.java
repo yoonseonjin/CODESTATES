f9b426c1d8bd4f69d9ebf20a32655856c3b4d740
hjs6877 <hjs6877@gmail.com>
2022-05-17 18:21:55 +0900

    first commit

---
+++ src/main/java/com/codestates/coffee/mapper/CoffeeMapper.java
@@ -0,0 +1,14 @@
+package com.codestates.coffee.mapper;
+
+import com.codestates.coffee.dto.CoffeePatchDto;
+import com.codestates.coffee.dto.CoffeePostDto;
+import com.codestates.coffee.dto.CoffeeResponseDto;
+import com.codestates.coffee.entity.Coffee;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CoffeeMapper {
+    Coffee coffeePostDtoToCoffee(CoffeePostDto coffeePostDto);
+    Coffee coffeePatchDtoToCoffee(CoffeePatchDto coffeePatchDto);
+    CoffeeResponseDto coffeeToCoffeeResponseDto(Coffee coffee);
+}
