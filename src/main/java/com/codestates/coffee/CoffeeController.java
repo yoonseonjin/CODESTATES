41eed2f0dd3209fff6c69ebbb993ed01963444c9
hjs6877 <hjs6877@gmail.com>
2022-05-17 14:20:19 +0900

    first commit

---
+++ src/main/java/com/codestates/coffee/CoffeeController.java
@@ -0,0 +1,57 @@
+package com.codestates.coffee;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+
+@RestController
+@RequestMapping("/v5/coffees")
+@Validated
+public class CoffeeController {
+    @PostMapping
+    public ResponseEntity postCoffee(@Valid @RequestBody CoffeePostDto coffeePostDto) {
+        // TODO CoffeeService 클래스와 연동하세요.
+        // TODO DTO <-> Entity 변환 Mapper를 적용하세요.
+
+        return new ResponseEntity<>(coffeePostDto, HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{coffee-id}")
+    public ResponseEntity patchCoffee(@PathVariable("coffee-id") @Positive long coffeeId,
+                                      @Valid @RequestBody CoffeePatchDto coffeePatchDto) {
+        coffeePatchDto.setCoffeeId(coffeeId);
+
+        // TODO CoffeeService 클래스와 연동하세요.
+        // TODO DTO <-> Entity 변환 Mapper를 적용하세요.
+
+        return new ResponseEntity<>(coffeePatchDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/{coffee-id}")
+    public ResponseEntity getCoffee(@PathVariable("coffee-id") long coffeeId) {
+        // TODO CoffeeService 클래스와 연동하세요.
+        // TODO DTO <-> Entity 변환 Mapper를 적용하세요.
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity getCoffees() {
+        // TODO CoffeeService 클래스와 연동하세요.
+        // TODO DTO <-> Entity 변환 Mapper를 적용하세요.
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{coffee-id}")
+    public ResponseEntity deleteCoffee(@PathVariable("coffee-id") long coffeeId) {
+        // TODO CoffeeService 클래스와 연동하세요.
+        // TODO DTO <-> Entity 변환 Mapper를 적용하세요.
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
