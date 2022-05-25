2d2ea2af9fa038391ee3841a58f381dfa9dfbb85
Lucky-kor <75019459+Lucky-kor@users.noreply.github.com>
2022-05-25 15:31:01 +0900

    Update main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,5 @@
 public class main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws JsonProcessingException {
     stringifyJSON test = new stringifyJSON();
     System.out.println(test.stringify("Hello CodeStates!"));
   }
