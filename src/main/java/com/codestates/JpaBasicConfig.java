732a31623f517929340b20efda53b75ce3677cc6
hjs6877 <hjs6877@gmail.com>
2022-06-07 09:22:12 +0900

    first commit

---
+++ src/main/java/com/codestates/JpaBasicConfig.java
@@ -0,0 +1,27 @@
+package com.codestates;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+
+@Configuration
+public class JpaBasicConfig {
+    private EntityManager em;
+    private EntityTransaction tx;
+
+
+    @Bean
+    public CommandLineRunner testJpaBasicRunner(EntityManagerFactory emFactory) {
+        this.em = emFactory.createEntityManager();
+        this.tx = em.getTransaction();
+
+        System.out.println("# Active Profile: many-to-one");
+        return args -> {
+            // TODO 이 곳에서 학습할 코드를 타이핑하세요!
+        };
+    }
+}
