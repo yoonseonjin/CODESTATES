fc18e517d55d986fbe78d8e50a81d3ac6d3e1f94
hjs6877 <hjs6877@gmail.com>
2022-06-27 18:11:36 +0900

    Modify API Layer

--- src/main/java/com/codestates/member/dto/MemberPatchDto.java
+++ src/main/java/com/codestates/member/dto/MemberPatchDto.java
@@ -19,16 +19,8 @@ public class MemberPatchDto {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getPhone() {
         return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
     }
 
     public long getMemberId() {
