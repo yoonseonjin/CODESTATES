b441581c561258e3ee33533643eba6375206ef68
hjs6877 <hjs6877@gmail.com>
2022-07-18 10:57:36 +0900

    Template Reactive

---
+++ src/main/java/com/codestates/Section4Week3TemplateReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.codestates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section4Week3TemplateReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section4Week3TemplateReactiveApplication.class, args);
+	}
+
+}
