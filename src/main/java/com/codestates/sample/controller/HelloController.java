7c308e3e290a2090a7f5ebe2a43a75f490b702d7
gimseyun <seyoonkim48@gmail.com>
2022-07-18 22:57:55 +0900

    add : print pid

--- src/main/java/com/codestates/sample/controller/HelloController.java
+++ src/main/java/com/codestates/sample/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.codestates.sample.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ public class HelloController {
 
   @GetMapping("/")
   public String hello() {
-    return "Hello World, Hello BE BootCamp!";
+    long pid = ProcessHandle.current().pid();
+    return String.format("Hello World, Hello BE Bootcamp! @PID : %d", pid);
   }
 }
