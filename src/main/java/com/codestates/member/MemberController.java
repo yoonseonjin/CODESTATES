f5c87bcfdb74cf6bd8152e7f25a3c368e11b5b71
hjs6877 <hjs6877@gmail.com>
2022-08-16 10:14:25 +0900

    Modify MemberController

--- src/main/java/com/codestates/member/MemberController.java
+++ src/main/java/com/codestates/member/MemberController.java
@@ -4,12 +4,27 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/members")
 public class MemberController {
+    private final Map<Long, Map<String, Object>> members = new HashMap<>();
+
+    @PostConstruct
+    public void init() {
+        Map<String, Object> member1 = new HashMap<>();
+        long memberId = 1L;
+        member1.put("memberId", memberId);
+        member1.put("email", "hgd@gmail.com");
+        member1.put("name", "홍길동");
+        member1.put("phone", "010-1234-5678");
+
+        members.put(memberId, member1);
+    }
+
     @PostMapping
     public ResponseEntity postMember(@RequestParam("email") String email,
                                      @RequestParam("name") String name,
