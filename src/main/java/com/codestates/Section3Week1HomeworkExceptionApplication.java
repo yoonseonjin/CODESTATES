64c194b1f569a81efa70df35a660c5f70df80685
hjs6877 <hjs6877@gmail.com>
2022-11-22 10:56:48 +0900

    .

--- src/main/java/com/codestates/Section3Week1HomeworkExceptionApplication.java
+++ src/main/java/com/codestates/Section3Week1HomeworkExceptionApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Be39Section3Week1HomeworkExceptionApplication {
+public class Section3Week1HomeworkExceptionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Be39Section3Week1HomeworkExceptionApplication.class, args);
+		SpringApplication.run(Section3Week1HomeworkExceptionApplication.class, args);
 	}
 
 }
--- src/test/java/com/codestates/Section3Week1HomeworkExceptionApplicationTests.java
+++ src/test/java/com/codestates/Section3Week1HomeworkExceptionApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Be39Section3Week1HomeworkExceptionApplicationTests {
+class Section3Week1HomeworkExceptionApplicationTests {
 
 	@Test
 	void contextLoads() {
