c4eb87e98bbb4888a4fec28c0df4b43c57a90e28
hjs6877 <hjs6877@gmail.com>
2023-01-11 18:50:47 +0900

    .

--- src/main/java/com/codestates/member/dto/MemberDto.java
+++ src/main/java/com/codestates/member/dto/MemberDto.java
@@ -49,7 +49,7 @@ public class MemberDto {
 
     @AllArgsConstructor
     @Getter
-    public static class response {
+    public static class Response {
         private long memberId;
         private String email;
         private String name;
