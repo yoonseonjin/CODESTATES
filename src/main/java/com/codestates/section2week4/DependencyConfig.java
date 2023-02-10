3599d78ef87f776e991e22625b1704d0b8dca107
yoonseonjin <xeccior@gmail.com>
2023-02-11 04:03:20 +0900

    singleton

--- src/main/java/com/codestates/section2week4/DependencyConfig.java
+++ src/main/java/com/codestates/section2week4/DependencyConfig.java
@@ -4,20 +4,27 @@ import com.codestates.section2week4.coffee.CoffeeRepository;
 import com.codestates.section2week4.coffee.CoffeeService;
 import com.codestates.section2week4.member.MemberRepository;
 import com.codestates.section2week4.member.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DependencyConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemberRepository();
     }
 
+    @Bean
     public CoffeeService coffeeService() {
         return new CoffeeService(coffeeRepository());
     }
 
+    @Bean
     public CoffeeRepository coffeeRepository() {
         return new CoffeeRepository();
     }
