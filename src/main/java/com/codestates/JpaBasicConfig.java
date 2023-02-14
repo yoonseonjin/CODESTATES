329a9bf6e56aa06f4a837330cdeb6b7a93706ba7
hjs6877 <hjs6877@gmail.com>
2023-02-15 01:18:33 +0900

    Entity Mapping

--- src/main/java/com/codestates/JpaBasicConfig.java
+++ src/main/java/com/codestates/JpaBasicConfig.java
@@ -19,6 +19,7 @@ public class JpaBasicConfig {
         this.em = emFactory.createEntityManager();
         this.tx = em.getTransaction();
 
+        System.out.println("# Active Profile: many-to-one");
         return args -> {
             // TODO 이 곳에 학습할 코드를 타이핑하세요!
         };
